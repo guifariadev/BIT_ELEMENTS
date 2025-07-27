82d363bdb5554f01a0f97b44af45bd40e76abb5e
guifariadev <guils46.faria@gmail.com>
2025-07-27 20:58:44 -0300

    fix: test skeleton 301 react

--- src/app/docs/LoadingWrapper.jsx
+++ src/app/docs/LoadingWrapper.jsx
@@ -1,20 +1,11 @@
 "use client";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
 import SkeletonDocs from "@/components/content/Skeleton";
 
 export default function LoadingWrapper({ children }) {
-  const pathname = usePathname();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 300);
-
-    return () => clearTimeout(timer);
-  }, [pathname]);
-
-  return isLoading ? <SkeletonDocs /> : children;
+  return (
+    <Suspense fallback={<SkeletonDocs />}>
+      {children}
+    </Suspense>
+  );
 }
