3bb2e6d071536f6e878fa87084edd7272433b9dd
guifariadev <guils46.faria@gmail.com>
2025-07-28 20:24:25 -0300

    feat: copy to clipboard

--- src/app/docs/[slug]/page.jsx
+++ src/app/docs/[slug]/page.jsx
@@ -7,6 +7,7 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { MdCode } from "react-icons/md";
 import { FaRegEye } from "react-icons/fa6";
 import { useEffect, useState } from "react";
+import { Clipboard, Check } from "lucide-react";
 
 export default function DocsPage() {
   const { slug } = useParams();
@@ -16,6 +17,10 @@ export default function DocsPage() {
   const DemoComponent = Demos?.[component.demo];
   const [demoCode, setDemoCode] = useState("");
   const [codeSource, setCodeSource] = useState("");
+  // State to manage clipboard copy status
+  const [copiedDemo, setCopiedDemo] = useState(false);
+  const [copiedSource, setCopiedSource] = useState(false);
+  const [copiedInstall, setCopiedInstall] = useState(false);
 
   // Load demo code via raw-loader
   useEffect(() => {
@@ -37,6 +42,14 @@ export default function DocsPage() {
     };
     loadSourceCode();
   }, [component.source]);
+
+  // Function to copy text to clipboard
+  const copyToClipboard = (text, setCopied) => {
+    navigator.clipboard.writeText(text).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    });
+  };
 
   return (
     <div className="max-w-4xl mx-auto px-4 text-white mt-20">
@@ -69,17 +82,25 @@ export default function DocsPage() {
             )}
           </div>
         </TabsContent>
-
+        {/* demo code */}
         <TabsContent value="code">
-          <CodeBlock
-            showLineNumbers
-            customStyle={{
-              borderRadius: "0.5rem",
-              padding: "1rem",
-            }}
-            code={demoCode}
-            language="javascript"
-          />
+          <div className="relative">
+            <CodeBlock
+              showLineNumbers
+              customStyle={{
+                borderRadius: "0.5rem",
+                padding: "1rem",
+              }}
+              code={demoCode}
+              language="javascript"
+            />
+            <button
+              onClick={() => copyToClipboard(demoCode, setCopiedDemo)}
+              className="absolute top-2 right-2 bg-zinc-700 hover:bg-zinc-600 p-2 rounded transition cursor-pointer"
+            >
+              {copiedDemo ? <Check className="size-4"/> : <Clipboard className="size-4"/> }
+            </button>
+          </div>
         </TabsContent>
       </Tabs>
 
@@ -87,12 +108,17 @@ export default function DocsPage() {
         <h2 className="text-xl font-bold mt-10 mb-4">Installation</h2>
         <p className="text-zinc-400 text-sm mb-2">Install dependencies</p>
         {component.install.map((cmd, i) => (
-          <pre
-            key={i}
-            className="bg-zinc-900 border border-zinc-800 rounded px-4 py-2 mb-2 text-sm text-wrap"
-          >
-            {cmd}
-          </pre>
+          <div key={i} className="relative mb-2">
+            <pre className="bg-zinc-900 border border-zinc-800 rounded px-4 py-2 text-sm text-wrap">
+              {cmd}
+            </pre>
+            <button
+              onClick={() => copyToClipboard(cmd, setCopiedInstall)}
+              className="absolute top-2 right-2 bg-zinc-700 hover:bg-zinc-600 p-1 rounded transition cursor-pointer"
+            >
+              {copiedInstall ? <Check className="size-4"/> : <Clipboard className="size-4"/> }
+            </button>
+          </div>
         ))}
       </div>
       {/* source code view */}
@@ -101,15 +127,23 @@ export default function DocsPage() {
         <p className="text-zinc-400 mb-4 text-xs bg-zinc-800 w-fit px-2 py-1 rounded-xs">
           {component.sourcePath}
         </p>
-        <CodeBlock
-          showLineNumbers
-          customStyle={{
-            borderRadius: "0.5rem",
-            padding: "1rem",
-          }}
-          code={codeSource}
-          language="javascript"
-        />
+        <div className="relative">
+          <CodeBlock
+            showLineNumbers
+            customStyle={{
+              borderRadius: "0.5rem",
+              padding: "1rem",
+            }}
+            code={codeSource}
+            language="javascript"
+          />
+          <button
+              onClick={() => copyToClipboard(codeSource, setCopiedSource)}
+              className="absolute top-2 right-2 bg-zinc-700 hover:bg-zinc-600 text-xs p-2 rounded transition cursor-pointer"
+            >
+              {copiedSource ? <Check className="size-4 "/> : <Clipboard className="size-4"/>}
+            </button>
+        </div>
       </div>
     </div>
   );
